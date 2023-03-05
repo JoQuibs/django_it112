062592979163480fa8d95db4ec5fee403aa0d6d2
Jomar Quiban <104065461+JoQuibs@users.noreply.github.com>
2023-03-05 07:17:32 -0800

    Update urls.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.main, name='main'),
     path('django_project/', views.django_project, name='django_project'),
     path('django_project/details/<int:id>', views.details, name='details'),
